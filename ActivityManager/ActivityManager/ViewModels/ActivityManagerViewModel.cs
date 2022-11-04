f135f1fc61faa4ba7c74988476ece76bf9257abb
AndersLind99 <cph-al350@cphbusiness.dk>
2022-11-04 14:11:00 +0100

    push before rework

--- ActivityManager/ActivityManager/ViewModels/ActivityManagerViewModel.cs
+++ ActivityManager/ActivityManager/ViewModels/ActivityManagerViewModel.cs
@@ -3,50 +3,55 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace ActivityManager.ViewModels
 {
-    public class ActivityManagerViewModel: BaseViewModel
+
+    public class ActivityManagerViewModel : BaseViewModel, INotifyPropertyChanged
     {
+        public EventHandler TimeStampsUpdate;
+        public ICommand ButtonPressedCommand
+        {
+            get; private set;
+        }
+
         private ObservableCollection<string> timeStamps;
 
         public ObservableCollection<string> TimeStamps
         {
 
-            get => timeStamps; set => SetProperty(ref timeStamps,value);
+            get => timeStamps; set => SetProperty(ref timeStamps, value);
         }
 
-        public String TimeNow
-        {
-            get; set;
-        }
 
         public ActivityManagerViewModel()
         {
+            ButtonPressedCommand = new Command(() => ButtonPressed());
 
 
+            TimeStamps = new ObservableCollection<string>();
 
-
-            TimeStamps = new ObservableCollection<string> { };
             for (int i = 0; i < 24; i++)
             {
                 var stringTimeStamp = i.ToString() + ":00";
                 TimeStamps.Insert(i, stringTimeStamp);
             }
 
-
-
-
-
-
-
+           
 
 
         }
 
+        private void ButtonPressed()
+        {
+
+            timeStamps.Add("test");
+            TimeStampsUpdate?.Invoke(this,null);
 
 
+        }
 
 
 
--- ActivityManager/ActivityManager/Views/ActivityManagerView.xaml.cs
+++ ActivityManager/ActivityManager/Views/ActivityManagerView.xaml.cs
@@ -1,37 +1,81 @@
-﻿using System;
+﻿using ActivityManager.ViewModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Xml.Linq;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 namespace ActivityManager.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ActivityManagerView : ContentView
     {
+        ActivityManagerViewModel vm;
         public ActivityManagerView()
         {
+
             InitializeComponent();
+            vm = BindingContext as ActivityManagerViewModel;
+
+            //   AbsoluteLayout.SetLayoutFlags(overlay, AbsoluteLayout.GetLayoutFlags(underlay));
+            // AbsoluteLayout.SetLayoutBounds(overlay, AbsoluteLayout.GetLayoutBounds(underlay));
+
+            TimeStampLabels(this,null);
+
+            vm.TimeStampsUpdate += TimeStampLabels;
+
+          
 
 
-
-            AbsoluteLayout.SetLayoutFlags(overlay, AbsoluteLayout.GetLayoutFlags(underlay));
-            AbsoluteLayout.SetLayoutBounds(overlay, AbsoluteLayout.GetLayoutBounds(underlay));
         }
 
-        BoxView red;
-        private void Whatever_SizeChanged(object sender, EventArgs e)
+        
+
+
+        private void TimeStampLabels(object sender, EventArgs e)
         {
+            underlay.Children.Clear();
+            for (int i = 0; i < vm.TimeStamps.Count; i++)
+            {
+
+                Label timeStampLabel = new Label { Text = vm.TimeStamps[i] };
+                underlay.Children.Add(timeStampLabel);
+
+            }
+
+          //  OnPropertyChanged(nameof(underlay));
 
 
-            red = new BoxView { Color = Color.Red };
-            overlay.Children.Add(red);
-            ////Console.WriteLine("////////// Red 1" + red.X + " " + red.Y + " " + red.Width + " " + red.Height);
-            ////Console.WriteLine("////////// Whatever 1" + label.X + " " + label.Y + " " + label.Width + " " + label.Height);
-            var width = container.Width - label.Width;
-            var rect = new Rectangle(container.X + label.Width + (width / 10), container.Y, width - (width / 10), container.Height);
-            red.LayoutTo(rect);
-
-            //Console.WriteLine("////////// Red 2" + red.X + " " + red.Y + " " + red.Width + " " + red.Height);
-            //Console.WriteLine("////////// Whatever 2" + label.X + " " + label.Y + " " + label.Width + " " + label.Height);
         }
+
+        //   BoxView red;
+
+
+        //private void RandomEvent(object sender, EventArgs e)
+        //{
+        //    Label timeStamp = sender as Label;
+        //    Debug.WriteLine(timeStamp.Text);
+
+
+        //    red = new BoxView { Color = Color.Red };
+        //    overlay.Children.Add(red);
+        //    Console.WriteLine("////////// Red 1" + red.X + " " + red.Y + " " + red.Width + " " + red.Height);
+        //    Console.WriteLine("////////// Whatever 1" + timeStamp.X + " " + timeStamp.Y + " " + timeStamp.Width + " " + timeStamp.Height);
+        //    var width = container.Width - timeStamp.Width;
+        //    //  var rect = new Rectangle(container.X + timeStamp.Width + (width / 10), container.Y, width - (width / 10), container.Height);
+        //    var rect = new Rectangle(timeStamp.X, timeStamp.Y, timeStamp.Width, timeStamp.Height);
+        //    red.LayoutTo(rect);
+        //    Console.WriteLine("////////// Red 1" + red.X + " " + red.Y + " " + red.Width + " " + red.Height);
+        //    Console.WriteLine("////////// Whatever 1" + timeStamp.X + " " + timeStamp.Y + " " + timeStamp.Width + " " + timeStamp.Height);
+
+
+        //}
 
 
     }
