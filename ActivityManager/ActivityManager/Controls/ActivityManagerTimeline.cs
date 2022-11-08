c8ba8dbfa8f92d227b4de5fb54d5364aa069e702
AndersLind99 <cph-al350@cphbusiness.dk>
2022-11-08 13:56:31 +0100

    major rework to control

--- ActivityManager/ActivityManager/Controls/ActivityManagerTimeline.cs
+++ ActivityManager/ActivityManager/Controls/ActivityManagerTimeline.cs
@@ -9,36 +9,80 @@ using Xamarin.Forms;
 
 namespace ActivityManager.Controls
 {
-
-
-
     public class ActivityManagerTimeline : ContentView
     {
 
-        public EventHandler TimeStampsUpdate;
+        #region bindable properties
+        public static readonly BindableProperty ActivityTimelineProperty =
+           BindableProperty.Create(nameof(ActivityTimeline), typeof(ObservableCollection<string>), typeof(ActivityManagerTimeline),
+               defaultValue: (new ObservableCollection<string>()), BindingMode.OneWay, propertyChanged: ActivityTimelineChanged);
+
+        public ObservableCollection<string> ActivityTimeline
+        {
+            get
+            {
+                return (ObservableCollection<string>)GetValue(ActivityTimelineProperty);
+            }
+            set
+            {
+                SetValue(ActivityTimelineProperty, value);
+                CreateActivityTimeline();
+
+            }
+        }
+
+        private static void ActivityTimelineChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var obj = ((ActivityManagerTimeline)bindable);
+            if (obj.ActivityTimeline != null)
+                obj.ActivityTimeline = (ObservableCollection<string>)newValue;
+        }
+        #endregion
+
+
+        #region commands
+
         public ICommand ButtonPressedCommand
         {
             get; private set;
         }
+        #endregion
 
-        private ObservableCollection<string> timeStamps;
-
-        public ObservableCollection<string> TimeStamps
+        private ObservableCollection<string> Timestamps
         {
-
-            get => timeStamps; set => timeStamps = value;
+            get; set;
         }
 
+
+        AbsoluteLayout absoluteLayout;
+
         StackLayout underlay;
+
+        StackLayout overlay;
+
 
         public ActivityManagerTimeline()
         {
 
+
             ButtonPressedCommand = new Command(() => ButtonPressed());
-          
+
             // TODO: MOVE VIEWMODEL & CODE BEHIND TO THIS CONTROL!!!!
             CreateDefaultContentPage();
-          //  DefaultTimestamps();
+            DefaultTimestamps();
+
+
+            Content = absoluteLayout;
+        }
+
+
+        public void CreateActivityTimeline()
+        {
+
+            ListView listView = new ListView { ItemsSource = ActivityTimeline };
+
+            underlay.Children.Add(listView);
+
 
         }
 
@@ -53,22 +97,37 @@ namespace ActivityManager.Controls
                 BackgroundColor = Color.GreenYellow,
 
 
+
             };
-          //  AbsoluteLayout.SetLayoutFlags(underlay, AbsoluteLayoutFlags.All);
-         //   AbsoluteLayout.SetLayoutBounds(underlay, new Rectangle(0, 0, 1, 1));
+            AbsoluteLayout.SetLayoutFlags(underlay, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(underlay, new Rectangle(0, 0, 1, 1));
+
+            overlay = new StackLayout()
+            {
+                BackgroundColor = Color.AntiqueWhite
 
 
-            Content = new AbsoluteLayout
+
+            };
+
+
+            absoluteLayout = new AbsoluteLayout
             {
                 VerticalOptions = LayoutOptions.FillAndExpand,
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 BackgroundColor = Color.Beige,
+                BindingContext = ActivityTimeline,
 
 
-                Children = { new Button {
-                    BorderColor = Color.Black, BackgroundColor = Color.Blue, Command = ButtonPressedCommand, AnchorX = 10 },
+                Children = {
 
-                underlay
+                    underlay,
+
+
+                    new Button {
+                    BorderColor = Color.Black,
+                        BackgroundColor = Color.Blue,
+                        Command = ButtonPressedCommand, AnchorX = 10 }
 
                 }
 
@@ -87,9 +146,11 @@ namespace ActivityManager.Controls
         {
 
             var buttonPressedLabel = new Label { Text = "you pressed the button" };
+            ActivityTimeline.Add("hihihi");
 
-            underlay.Children.Add(buttonPressedLabel);
-            
+
+
+
 
 
         }
@@ -99,15 +160,12 @@ namespace ActivityManager.Controls
         private void DefaultTimestamps()
         {
 
-
-
             for (int i = 0; i < 24; i++)
             {
-                Label timeStamp = new Label { Text = i.ToString() + ":00" };
-                underlay.Children.Add(timeStamp);
+
+                ActivityTimeline.Add(i.ToString() + ":00");
 
             }
-
 
 
         }
